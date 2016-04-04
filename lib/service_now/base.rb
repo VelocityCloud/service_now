fc0d6bfa5742d4258917a73a066c348f7abba4ff
Diego Carrion <dc.rec1@gmail.com>
2016-04-04 12:41:45 -0300

    make some method private

--- lib/service_now/base.rb
+++ lib/service_now/base.rb
@@ -5,36 +5,40 @@ module ServiceNow
     include HTTParty
     headers 'Accept' => 'application/json', 'Content-Type' => 'application/json'
 
-    def self.all(params = {}, auth)
-      exec :get, "?#{build_query(params)}", {}, auth
-    end
+    class << self
+      def all(params = {}, auth)
+        exec :get, "?#{build_query(params)}", {}, auth
+      end
 
-    def self.find(sys_id, auth)
-      exec :get, "/#{sys_id}", {}, auth
-    end
+      def find(sys_id, auth)
+        exec :get, "/#{sys_id}", {}, auth
+      end
 
-    def self.create(params, auth)
-      exec :post, "", params, auth
-    end
+      def create(params, auth)
+        exec :post, "", params, auth
+      end
 
-    def self.update(sys_id, params, auth)
-      exec :patch, "/#{sys_id}", params, auth
-    end
+      def update(sys_id, params, auth)
+        exec :patch, "/#{sys_id}", params, auth
+      end
 
-    def self.url(auth)
-      "https://#{auth[:host]}/api/now/table/#{table_name}"
-    end
+      private
 
-    def self.table_name
-      to_s.downcase.split('::').last
-    end
+      def url(auth)
+        "https://#{auth[:host]}/api/now/table/#{table_name}"
+      end
 
-    def self.exec(method, path, params, auth)
-      send(method, "#{url(auth)}#{path}", body: params.to_json, basic_auth: { username: auth[:user], password: auth[:password] })['result']
-    end
+      def table_name
+        to_s.downcase.split('::').last
+      end
 
-    def self.build_query(params)
-      params.to_a.map { |name, value| "#{name}=#{value}" }.join('&')
+      def exec(method, path, params, auth)
+        send(method, "#{url(auth)}#{path}", body: params.to_json, basic_auth: { username: auth[:user], password: auth[:password] })['result']
+      end
+
+      def build_query(params)
+        params.to_a.map { |name, value| "#{name}=#{value}" }.join('&')
+      end
     end
   end
 end
