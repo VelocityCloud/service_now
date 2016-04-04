da2eb4c47e1508a6ce4aab8a59b3687313f0b0f2
Diego Carrion <dc.rec1@gmail.com>
2016-04-04 10:08:23 -0300

    feat: add ServiceNow::Incident.create

--- lib/service_now/base.rb
+++ lib/service_now/base.rb
@@ -3,18 +3,26 @@ require 'httparty'
 module ServiceNow
   class Base
     include HTTParty
-    headers 'Accept' => 'application/json'
+    headers 'Accept' => 'application/json', 'Content-Type' => 'application/json'
 
     def self.find(sys_id, auth)
-      get("#{url(auth)}/api/now/table/#{table_name}/#{sys_id}", basic_auth: { username: auth[:user], password: auth[:password] })['result']
+      exec :get, "/#{sys_id}", {}, auth
+    end
+
+    def self.create(params, auth)
+      exec :post, "", params, auth
     end
 
     def self.url(auth)
-      "https://#{auth[:host]}"
+      "https://#{auth[:host]}/api/now/table/#{table_name}"
     end
 
     def self.table_name
       to_s.downcase.split('::').last
+    end
+
+    def self.exec(method, path, params, auth)
+      send(method, "#{url(auth)}#{path}", body: params.to_json, basic_auth: { username: auth[:user], password: auth[:password] })['result']
     end
   end
 end
--- spec/incident_spec.rb
+++ spec/incident_spec.rb
@@ -1,10 +1,28 @@
 require 'spec_helper'
 
 describe ServiceNow::Incident do
+  let(:auth) { { host: ENV['SN_HOST'], user: ENV['SN_USER'], password: ENV['SN_PASSWORD'] } }
+
   describe '.find' do
     it 'finds a record by the sys id' do
       VCR.use_cassette('incident') do
-        expect(ServiceNow::Incident.find('7c7cdfc1136ed6400465bd122244b0d0', host: ENV['SN_HOST'], user: ENV['SN_USER'], password: ENV['SN_PASSWORD'])['number']).to eql('INC0726249')
+        expect(ServiceNow::Incident.find('7c7cdfc1136ed6400465bd122244b0d0', auth)['number']).to eql('INC0726249')
+      end
+    end
+  end
+
+  describe '.create' do
+    it 'creates a record' do
+      VCR.use_cassette('create_incident') do
+        params =  { 
+          :active => 'false',
+          :category => 'network',
+          :description => 'User cannot access email on mail.company.com.',
+          :knowledge => 'false',
+          :made_sla => 'false',
+          :short_description => 'Cannot read email'
+        } 
+        expect(ServiceNow::Incident.create(params, auth)['sys_id']).to eql('9719345913621ec0fef07d322244b058')
       end
     end
   end
